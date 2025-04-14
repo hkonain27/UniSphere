2e17d41e6b19256f0ad05636283a30bf4f803528
Hafsa Konain <hkonain@uncc.edu>
2025-04-14 14:21:01 -0400

    Commit changes

--- settings.py
+++ settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'UniSphere.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+        BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
