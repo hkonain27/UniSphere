1858ca9c51b115585b0e9a8fc2e6c5a32c59c90c
Hafsa Konain <hkonain@uncc.edu>
2025-04-23 20:39:11 -0400

    commit changes

--- UniSphere/urls.py
+++ UniSphere/urls.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 
-# def home(request):
-#     return HttpResponse('Home page')
-
-# def room(request):
-#     return HttpResponse('ROOM')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls'))
-    # path('', home),
-    # path('room/', room)
+    
 ]
